1bcf872409c512d58fc0a0e1f1b9ab07436f6ea1
tranduythuat <thuattran1810@gmail.com>
2026-06-14 02:30:00 +0700

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -149,11 +149,11 @@
     openCard.addEventListener("click", () => {
       markCardOpened();
 
-      if (audio && audio.paused) {
-        audio.play().catch(err => {
-          console.log("Autoplay blocked:", err);
-        });
-      }
+      // if (audio && audio.paused) {
+      //   audio.play().catch(err => {
+      //     console.log("Autoplay blocked:", err);
+      //   });
+      // }
       tl.play();
     });
   }
